c9f03edbfc7986517ed305853c8e37d202900b9b
Piero <piero@piero.com>
2018-11-29 12:49:16 +0100

    Maquetacion home,login, register

--- ang-login-firebase/src/environments/environment.prod.ts
+++ ang-login-firebase/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBvVfN7oxvcrLtHq7affjus4BZH1BjWIV8",
+    authDomain: "angular-todo-c7801.firebaseapp.com",
+    databaseURL: "https://angular-todo-c7801.firebaseio.com",
+    projectId: "angular-todo-c7801",
+    storageBucket: "angular-todo-c7801.appspot.com",
+    messagingSenderId: "438891185906"
+  }
 };
