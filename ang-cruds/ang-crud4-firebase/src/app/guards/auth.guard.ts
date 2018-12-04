c13009502c9a750a6847919664b9f8ac60585ec0
Piero Vasquez <piero.vasquez.r@gmail.com>
2018-12-04 12:35:29 +0100

    procesando

--- ang-cruds/ang-crud4-firebase/src/app/guards/auth.guard.ts
+++ ang-cruds/ang-crud4-firebase/src/app/guards/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot,Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, take, tap } from 'rxjs/operators';
 
 import { AuthService } from '../services/auth.service'
 
@@ -21,7 +21,7 @@ export class AuthGuard implements CanActivate {
     return this.service.afAuth.authState
     .pipe(take(1))
     .pipe(map( authState => !! authState))
-    .pipe(tag(logado => {
+    .pipe(tap(logado => {
       if (!logado) {
         this.router.navigate(['/login']);
       }
