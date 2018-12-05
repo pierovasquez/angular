2a93d623a6d8a625770b1e5c33f55924c1f744c2
Piero Vasquez <piero.vasquez.r@gmail.com>
2018-12-05 12:28:53 +0100

    Progresando con el CRUD4

--- ang-cruds/ang-crud4-firebase/src/app/components/details/details.component.ts
+++ ang-cruds/ang-crud4-firebase/src/app/components/details/details.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Params, ActivatedRoute } from '@angular/router';
+import { RecetaInterface } from '../../models/Receta';
+import { RecetaService } from '../../services/receta.service';
+import { Observable } from 'rxjs';
+import {AuthService} from '../../services/auth.service';
+
+
+
 
 @Component({
   selector: 'app-details',
@@ -7,9 +15,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  idReceta: string;
+  idUsuarioL: string;
+  isOwner: boolean = false;
+  receta: RecetaInterface = {
+    id: '',
+    titulo: '',
+    descripcion: '',
+    preparacion: '',
+    ingredientes: '',
+    temporada: '',
+    fechaPublicacion: '',
+    userId: '',
+    userName: ''
+  }
+
+
+  constructor(
+    private authservice: AuthService,
+    private recetaservice: RecetaService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.onComprobarUserLogin();
+    this.getDetailsReceta();
   }
+
+  onComprobarUserLogin() {
+    this.authservice.getAuth().subscribe(user =>{
+      if(user) {
+        this.idUsuarioL = user.uid;
+      }
+    })
+  }
+
+  getDetailsReceta() {
+    this.idReceta = this.route.snapshot.params['id'];
+    this.recetaservice.getOneReceta(this.receta).subscribe(receta =>{
+      this.receta = receta;
+      if(this.idUsuarioL == this.receta.userId) {
+        this.isOwner = true;
+      }
+    })
+  }
+
+  onClickDelete() {
+    if(confirm('Are you sure?')) {
+      this.recetaservice.deleteReceta(this.receta);
+      this.router.navigate(['/']);
+    }
+  }
+
+
+
+
 
 }
--- ang-cruds/ang-crud4-firebase/src/app/components/nueva-receta/nueva-receta.component.ts
+++ ang-cruds/ang-crud4-firebase/src/app/components/nueva-receta/nueva-receta.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { RecetaInterface } from '../../models/Receta';
+import { AuthService } from '../../services/auth.service';
+import { RecetaService } from '../../services/receta.service';
+import { Router} from '@angular/router'
+
 
 @Component({
   selector: 'app-nueva-receta',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevaRecetaComponent implements OnInit {
 
-  constructor() { }
+  receta:RecetaInterface = {
+     id: '',
+     titulo: '',
+     descripcion: '',
+     preparacion: '',
+     ingredientes: '',
+     temporada: '',
+     fechaPublicacion: '',
+     userId: '',
+     userName: ''
+
+  }
+
+  constructor(
+    private router: Router,
+    private recetaservice: RecetaService,
+    private authservice: AuthService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onGuardarReceta({value}: {value:RecetaInterface}) {
+    value.fechaPublicacion = (new Date()).getTime();
+    this.authservice.getAuth().subscribe( user => {
+      value.userId = user.uid;
+      value.userName = user.displayName;
+      this.recetaservice.addNewReceta(value);
+    });
+    this.router.navigate(['/']);
   }
 
 }
