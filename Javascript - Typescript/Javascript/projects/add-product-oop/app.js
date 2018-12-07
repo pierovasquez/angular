f0161a0c5fc219733d48918f53a2845cb2913a0f
Piero <piero.vasquez.r@gmail.com>
2018-12-07 22:37:39 +0100

    Inicios de Javascript

--- Javascript - Typescript/Javascript/projects/add-product-oop/app.js
+++ Javascript - Typescript/Javascript/projects/add-product-oop/app.js
@@ -19,7 +19,7 @@ class UI {
                 <strong>Product Name</strong>: ${product.name}
                 <strong>Product Price</strong>: ${product.price}
                 <strong>Product Year</strong>: ${product.year}
-                <a href="#" class="btn btn-danger">Delete</a>
+                <a href="#" class="btn btn-danger" name="delete">Delete</a>
             </div>
         </div>
         `;
@@ -27,12 +27,27 @@ class UI {
         productList.appendChild(element);
     }
 
-    deleteProduct() {
-
+    deleteProduct(element) {
+        // El elemento (boton) tiene como padre los divs, y hasta llegar al div inicial, seguimos llamando al parentElement
+        if(element.name === "delete") {
+            element.parentElement.parentElement.parentElement.remove();
+        }
+        this.showMessage('Product Deleted','info');
     }
 
-    showMessage() {
-
+    showMessage(message, cssClass) {
+        //Con el querySelector, podemos referirnos a las clases o a los id de los elementos que empiecen con la palabra introducida dentro del metodo.
+        const div = document.createElement('div');
+        div.className = `alert alert-${cssClass} mt-4`;
+        div.appendChild(document.createTextNode(message));
+        // Showing in DOM
+        const container = document.querySelector('.container');
+        const app = document.querySelector('#app');
+        // Indicams que dentro del container, insertamos el div pero antes del div con el id app.
+        container.insertBefore(div,app);
+        setTimeout(function() {
+            document.querySelector('.alert').remove();
+        }, 3000);
     }
 
     resetForm() {
@@ -52,8 +67,21 @@ document.getElementById('product-form')
 
         const ui = new UI();
 
+        //Si entra en el if, al poner un return, deja de leer el resto del codigo, si no pusiesemos el return, seguiria leyendo el codigo siguiente.
+        if(name === '' || price === '' || year === '') {
+            return ui.showMessage('Complete fields','danger')
+        }
+
         ui.addProduct(product);
         ui.resetForm();
+        ui.showMessage('Product Added', 'success');
+
         //preventDefault -> cancela el refresh del formulario al clicar en el submit
         e.preventDefault();
     });
+
+document.getElementById('product-list')
+    .addEventListener('click', function(e) {
+        const ui = new UI();
+        ui.deleteProduct(e.target);
+})
