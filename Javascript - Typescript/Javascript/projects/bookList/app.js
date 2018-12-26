981fad8c0b00741c67f5fcf7dd5e3fcd3965fbeb
Piero Vasquez <piero.vasquez.r@gmail.com>
2018-12-26 17:05:29 +0100

    Avanzando con el BookList

---
+++ Javascript - Typescript/Javascript/projects/bookList/app.js
@@ -0,0 +1,51 @@
+// Book Class: Represents a Book
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+// UI Class: Handle UI Tasks
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Book one',
+                author: 'Jhon Doe',
+                isbn: '3434334'
+            },
+            {
+                title: 'Book Two',
+                author: 'Jane Doe',
+                isbn: '454545445'
+            }
+        ];
+
+        const books = StoredBooks;
+
+        books.forEach((book) => UI.addBookToList(book));
+    }
+
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+        list.appendChild(row)
+    }
+}
+// Store Class: Handles Storage
+
+// Event: Display Books
+
+// Event: Add a book
+
+// Event: Remove a book
